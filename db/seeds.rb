2306c43dfa5ad7ae20aac82eb7f6c321d05e1794
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-09-27 15:16:55 -0600

    adds trip.csv to app outside hidden csv folder, stylizes both trip and stations dashboard, removes weather from r from seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,50 +35,17 @@ stations.each do |row|
 end
 system 'Say "Finished Seeding Stations"'
 
-binding.pry
-trips = CSV.open "./db/csv/trip.csv", headers: true, header_converters: :symbol
+trips = CSV.open "./db/trip.csv", headers: true, header_converters: :symbol
 trips.each do |row|
   row = row.to_h
   row[:start_date_id] = BikeShareDate.seed_by_date(row.delete(:start_date))
   row[:start_station_id] = Station.id_by_name(row.delete(:start_station_name))
   row[:end_date_id] = BikeShareDate.seed_by_date(row.delete(:end_date))
   row[:end_station_id] = Station.id_by_name(row.delete(:end_station_name))
-  row[:zipcode_id] = Zipcode.create_zipcode(row.delete(:zip_code))
+  row[:zipcode_id] = Zipcode.create_zipcode(row.delete(:zip_code)).id
   Trip.create!(row)
 end
 
 system 'Say "Finished Seeding Trips"'
-
-weather_conditions = delete_columns("./db/csv/weather.csv", [:max_dew_point_f,
-                                                      :mean_dew_point_f,
-                                                      :min_dew_point_f,
-                                                      :max_humidity,
-                                                      :min_humidity,
-                                                      :max_sea_level_pressure_inches,
-                                                      :mean_sea_level_pressure_inches,
-                                                      :min_sea_level_pressure_inches,
-                                                      :max_visibility_miles,
-                                                      :min_visibility_miles,
-                                                      :max_wind_speed_mph,
-                                                      :max_gust_speed_mph,
-                                                      :cloud_cover,
-                                                      :events,
-                                                      :wind_dir_degrees,
-                                                      :zip_code]
-                                                      )
-
-weather_conditions.each do |row|
-  row = row.to_h
-  row[:date_id] = BikeShareDate.seed_by_date(row.delete(:date))
-  row[:max_temp_f] = WeatherCondition.clean_float(row.delete(:max_temperature_f))
-  row[:mean_temp_f] = WeatherCondition.clean_float(row.delete(:mean_temperature_f))
-  row[:min_temp_f] = WeatherCondition.clean_float(row.delete(:min_temperature_f))
-  row[:mean_humidity] = WeatherCondition.clean_float(row.delete(:mean_humidity))
-  row[:mean_visibility] = WeatherCondition.clean_float(row.delete(:mean_visibility_miles))
-  row[:mean_wind_speed] = WeatherCondition.clean_float(row.delete(:mean_wind_speed_mph))
-  row[:precipitation] = WeatherCondition.clean_float(row.delete(:precipitation_inches))
-  WeatherCondition.create!(row)
-end
-system 'Say "Finished Seeding Weather Conditions"'
 
 puts Time.now - start_time
